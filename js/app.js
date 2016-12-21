7bf4c26ebc23953bedc444da65c8d2d41688682c
abbeyang <osilent.live.com>
2016-12-21 16:57:08 +0800

    change

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@
 angular.module("cartService",[]);
 
 // 创建一个主模块
-var myapp = angular.module("myapp",["ionic","cartService"]);
+var myapp = angular.module("myapp",["ionic","cartService", "starter.controllers", "starter.services"]);
 myapp.controller("myCtrl",function($scope,$http){
     var url="data.json";
     $http.get(url)
@@ -16,79 +16,75 @@ myapp.controller("myCtrl",function($scope,$http){
 
 // 配置路由
 myapp.config(function($stateProvider,$urlRouterProvider,$ionicConfigProvider){
-	$ionicConfigProvider.platform.ios.tabs.style('standard'); 
-        $ionicConfigProvider.platform.ios.tabs.position('bottom');
-        $ionicConfigProvider.platform.android.tabs.style('standard');
-        $ionicConfigProvider.platform.android.tabs.position('bottom');
+	  $ionicConfigProvider.platform.ios.tabs.style('standard');
+      $ionicConfigProvider.platform.ios.tabs.position('bottom');
+      $ionicConfigProvider.platform.android.tabs.style('standard');
+      $ionicConfigProvider.platform.android.tabs.position('standard');
 
-        $ionicConfigProvider.platform.ios.navBar.alignTitle('center'); 
-        $ionicConfigProvider.platform.android.navBar.alignTitle('center');
+      $ionicConfigProvider.platform.ios.navBar.alignTitle('center');
+      $ionicConfigProvider.platform.android.navBar.alignTitle('left');
 
-        $ionicConfigProvider.platform.ios.backButton.previousTitleText('').icon('ion-ios-arrow-thin-left');
-        $ionicConfigProvider.platform.android.backButton.previousTitleText('').icon('ion-android-arrow-back');        
+      $ionicConfigProvider.platform.ios.backButton.previousTitleText('').icon('ion-ios-arrow-thin-left');
+      $ionicConfigProvider.platform.android.backButton.previousTitleText('').icon('ion-android-arrow-back');
 
-        $ionicConfigProvider.platform.ios.views.transition('ios'); 
-        $ionicConfigProvider.platform.android.views.transition('android');
+      $ionicConfigProvider.platform.ios.views.transition('ios');
+      $ionicConfigProvider.platform.android.views.transition('android');
 
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
+      // Ionic uses AngularUI Router which uses the concept of states
+      // Learn more here: https://github.com/angular-ui/ui-router
+      // Set up the various states which the app can be in.
+      // Each state's controller can be found in controllers.js
+      $stateProvider
 
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: "/tab",
-    abstract: true,
-    templateUrl: "templates/tabs.html"
-  })
+        // setup an abstract state for the tabs directive
+          .state('tab', {
+            url: "/tab",
+            abstract: true,
+            templateUrl: "templates/tabs.html"
+          })
 
-  // Each tab has its own nav history stack:
+        // Each tab has its own nav history stack:
 
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
+          .state('tab.dash', {
+            url: '/dash',
+            views: {
+              'tab-dash': {
+                templateUrl: 'templates/tab-dash.html',
+                controller: 'DashCtrl'
+              }
+            }
+          })
 
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
+          .state('tab.chats', {
+            url: '/chats',
+            views: {
+              'tab-chats': {
+                templateUrl: 'templates/tab-chats.html',
+                controller: 'ChatsCtrl'
+              }
+            }
+          })
+          .state('tab.chat-detail', {
+            url: '/chats/:chatId',
+            views: {
+              'tab-chats': {
+                templateUrl: 'templates/chat-detail.html',
+                controller: 'ChatDetailCtrl'
+              }
+            }
+          })
 
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
-  });
-
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
-
-});
+          .state('tab.account', {
+            url: '/account',
+            views: {
+              'tab-account': {
+                templateUrl: 'templates/tab-account.html',
+                controller: 'AccountCtrl'
+              }
+            }
+          });
+          $urlRouterProvider.otherwise('/tab/dash');
     $stateProvider
         .state("tour",{
             url:"/tour",
