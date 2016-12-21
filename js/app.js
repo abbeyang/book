88d85cde1ea6b761eeeb62825fa25544a4c72f0f
abbeyang <osilent.live.com>
2016-12-21 17:30:01 +0800

    fix

--- js/app.js
+++ js/app.js
@@ -12,8 +12,6 @@ myapp.controller("myCtrl",function($scope,$http){
             console.log(data)
         })
 });
-
-
 // 配置路由
 myapp.config(function($stateProvider,$urlRouterProvider,$ionicConfigProvider){
 	  $ionicConfigProvider.platform.ios.tabs.style('standard');
@@ -30,61 +28,10 @@ myapp.config(function($stateProvider,$urlRouterProvider,$ionicConfigProvider){
       $ionicConfigProvider.platform.ios.views.transition('ios');
       $ionicConfigProvider.platform.android.views.transition('android');
 
-
       // Ionic uses AngularUI Router which uses the concept of states
       // Learn more here: https://github.com/angular-ui/ui-router
       // Set up the various states which the app can be in.
       // Each state's controller can be found in controllers.js
-      $stateProvider
-
-        // setup an abstract state for the tabs directive
-          .state('tab', {
-            url: "/tab",
-            abstract: true,
-            templateUrl: "templates/tabs.html"
-          })
-
-        // Each tab has its own nav history stack:
-
-          .state('tab.dash', {
-            url: '/dash',
-            views: {
-              'tab-dash': {
-                templateUrl: 'templates/tab-dash.html',
-                controller: 'DashCtrl'
-              }
-            }
-          })
-
-          .state('tab.chats', {
-            url: '/chats',
-            views: {
-              'tab-chats': {
-                templateUrl: 'templates/tab-chats.html',
-                controller: 'ChatsCtrl'
-              }
-            }
-          })
-          .state('tab.chat-detail', {
-            url: '/chats/:chatId',
-            views: {
-              'tab-chats': {
-                templateUrl: 'templates/chat-detail.html',
-                controller: 'ChatDetailCtrl'
-              }
-            }
-          })
-
-          .state('tab.account', {
-            url: '/account',
-            views: {
-              'tab-account': {
-                templateUrl: 'templates/tab-account.html',
-                controller: 'AccountCtrl'
-              }
-            }
-          });
-          $urlRouterProvider.otherwise('/tab/dash');
     $stateProvider
         .state("tour",{
             url:"/tour",
